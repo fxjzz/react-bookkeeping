6a2a75ef27346dd59efc2e218777c2be54f65d8d
lzxzz <1042183935@qq.com>
2022-10-06 21:20:31 +0800

    添加icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import React from "react";
 import {Link} from "react-router-dom";
+require('icons/tag.svg')
+require('icons/money.svg')
+require('icons/chart.svg')
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.3);
@@ -9,8 +12,15 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
       line-height: 24px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -20,13 +30,22 @@ const Nav = ()=>{
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="tags">标签页</Link>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"/>
+                    </svg>
+                    <Link to="tags">标签</Link>
                 </li>
                 <li>
-                    <Link to="money">记账页</Link>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
+                    <Link to="money">记账</Link>
                 </li>
                 <li>
-                    <Link to="statistics">统计页</Link>
+                    <svg className="icon">
+                        <use xlinkHref="#chart"/>
+                    </svg>
+                    <Link to="statistics">统计</Link>
                 </li>
             </ul>
         </NavWrapper>

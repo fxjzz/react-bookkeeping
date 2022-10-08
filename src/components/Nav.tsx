6284311851a4ef0b616c9a245cd0bbb60cfbee14
lzxzz <1042183935@qq.com>
2022-10-08 15:47:34 +0800

    添加money 的 html

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import Icon from "./Icon";
 
 
@@ -22,6 +22,9 @@ const NavWrapper = styled.nav`
           width: 24px;
           height: 24px;
         }
+        &.selected{
+          background: pink;
+        }
       }
     }
   }
@@ -32,22 +35,22 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="tags">
+                    <NavLink to="tags" activeClassName="selected">
                         <Icon name="tag"/>
                         标签
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="money">
+                    <NavLink to="money" activeClassName="selected">
                         <Icon name="money"/>
                         记账
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="statistics">
+                    <NavLink to="statistics" activeClassName="selected">
                         <Icon name="chart"/>
                         统计
-                    </Link>
+                    </NavLink>
                 </li>
             </ul>
         </NavWrapper>
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,9 +1,67 @@
 import Layout from "../components/Layout";
 import React from "react";
+import styled from "styled-components";
+
+const TagsSection = styled.section`
+
+`;
+const NotesSection = styled.section`
+
+`;
+const CategorySection = styled.section`
+
+`;
+const NumberPadSection = styled.section`
+
+`;
 
 function Money() {
     return (
-        <Layout>money</Layout>
+        <Layout>
+            <TagsSection>
+                <ul>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ul>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <label>
+                    <span>
+                        备注
+                    </span>
+                    <input type="text"/>
+                </label>
+            </NotesSection>
+            <CategorySection>
+                <ul>
+                    <li>支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div>100</div>
+                <div>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>OK</button>
+                    <button>0</button>
+                    <button>.</button>
+                </div>
+            </NumberPadSection>
+        </Layout>
     );
 }
-export default Money
+
+export default Money;

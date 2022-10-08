10704e36eeeff9d53850fb9683b459e06683b67e
lzxzz <1042183935@qq.com>
2022-10-08 21:34:17 +0800

    Tags 新增和选中

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
   flex-grow: 1;
@@ -8,9 +9,9 @@ const TagsSection = styled.section`
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
+
   > ol {
     margin: 0 -12px;
-
     > li {
       background: #D9D9D9;
       border-radius: 18px;
@@ -18,9 +19,11 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected {
+        background: pink;
+      }
     }
   }
-
   > button {
     background: none;
     border: none;
@@ -30,4 +33,38 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
-export default TagsSection
+const TagsSection: React.FC = (props) => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag);
+        if (index >= 0) {
+            setSelectedTags(selectedTags.filter(t => t !== tag)); // setSelectedTags!
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    };
+    const addTag = () => {
+        const tagName = window.prompt('请输入标签内容');
+        if (tagName !== null) {
+            setTags([...tags, tagName]);
+        }
+    };
+    const getClass=(tag:string)=> selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                    <li key={tag} onClick={
+                        () => {onToggleTag(tag);}
+                    } className={getClass(tag)}
+                    >{tag}</li>
+                )}
+            </ol>
+            <button onClick={addTag}>新增标签</button>
+        </Wrapper>
+    );
+};
+
+export default TagsSection;

52204d071d4ca548b8d56135932375bb3ec0872b
lzxzz <1042183935@qq.com>
2022-10-10 15:15:33 +0800

    路由使用exact

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,37 @@ import Money from "./views/Money";
 import Statistics from "./views/Statistics";
 import NoMatch from "./views/NoMatch";
 import styled from 'styled-components';
+import Tag from './views/Tag'
 
 const AppWrapper = styled.div`
   color: #333;
 `;
+
 function App() {
     return (
         <AppWrapper>
-        <Router>
-            <Switch>
-                <Route path="/tags">
-                    <Tags/>
-                </Route>
-                <Route path="/money">
-                    <Money/>
-                </Route>
-                <Route path="/statistics">
-                    <Statistics/>
-                </Route>
-                <Redirect exact from="/" to="/money"/>
-                <Route path="*">
-                    <NoMatch/>
-                </Route>
-            </Switch>
-        </Router>
+            <Router>
+                <Switch>
+                    <Route path="/tags" exact>
+                        <Tags/>
+                    </Route>
+                    <Route path="/tags/:tag" exact>
+                        <Tag/>
+                    </Route>
+                    <Route path="/money" exact>
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics" exact >
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from="/" to="/money"/>
+                    <Route path="*" exact >
+                        <NoMatch/>
+                    </Route>
+                </Switch>
+            </Router>
         </AppWrapper>
-    );
+);
 }
 
 export default App;
---
+++ src/views/Tag.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Tag:React.FC =()=>{
+    return (
+        <div>hi</div>
+    )
+}
+export default Tag

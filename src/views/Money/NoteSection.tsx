ff06bcad5af950749020af330082ad7bac5b688b
lzxzz <1042183935@qq.com>
2022-10-12 15:46:15 +0800

    Tag 组件样式完成

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -4,7 +4,7 @@ import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
 `;
 type Props = {
@@ -18,7 +18,8 @@ const NoteSection: React.FC<Props> = (props) => {
         };
         return (
             <Wrapper>
-                <Input label="备注" type="text" value={note} onChange={onChange}/>
+                <Input label="备注" type="text" value={note} onChange={onChange}
+                placeholder="请输入备注"/>
             </Wrapper>
         );
     };
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,18 +5,27 @@ import Layout from "../components/Layout";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
 import styled from "styled-components";
+import {Input} from "../components/Input";
+import Center from "../components/Center";
+import Space from "../components/Space";
 
 type Params = {
     id: string
 }
 const Topbar = styled.header`
-  display:flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   line-height: 20px;
   padding: 14px;
-  background:white;
+  background: white;
 `;
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
+
 const Tag: React.FC = (props) => {
     const {findTag} = useTags();
     let {id} = useParams<Params>();   //params 得到的id 是路由的:id 这是一个string
@@ -28,15 +37,15 @@ const Tag: React.FC = (props) => {
                 <span>编辑标签</span>
                 <Icon/>
             </Topbar>
-            <div>
-                <label>
-                    <span>标签名</span>
-                    <input type="text" placeholder="请输入"/>
-                </label>
-            </div>
-            <div>
+            <InputWrapper>
+                <Input label="标签名" type="text" placeholder="输入"/>
+            </InputWrapper>
+            <Center>
+                <Space/>
+                <Space/>
+                <Space/>
                 <Button>删除标签</Button>
-            </div>
+            </Center>
         </Layout>
     );
 };

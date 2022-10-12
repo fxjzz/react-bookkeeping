a026d4d342c5de391f5b301a2c5bcaa3955852ca
lzxzz <1042183935@qq.com>
2022-10-12 15:32:42 +0800

    Input

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,54 +1,25 @@
 import styled from "styled-components";
-import React, {useRef} from "react";
+import React, {ChangeEventHandler} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 type Props = {
     value:string,
     onChange:(value: string)=> void
 }
-const NoteSection:React.FC<Props> = (props) => {
-    const note =props.value
-    const refInput=useRef<HTMLInputElement>(null)
-    const onBlur =()=>{
-        if(refInput.current!==null){
-            props.onChange(refInput.current.value)
-        }
-    }
-    return (
-        <Wrapper>
-            <label>
-                    <span>
-                        备注
-                    </span>
-                <input type="text" placeholder="请输入备注"
-                       ref={refInput}
-                       defaultValue={note}
-                       onBlur={onBlur}
-                />
-            </label>
-        </Wrapper>
-    );
-};
+const NoteSection: React.FC<Props> = (props) => {
+        const note = props.value;
+        const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+            props.onChange(e.target.value);
+        };
+        return (
+            <Wrapper>
+                <Input label="备注" type="text" value={note} onChange={onChange}/>
+            </Wrapper>
+        );
+    };
 export default NoteSection;
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -4,24 +4,34 @@ import useTags from "../useTags";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
+import styled from "styled-components";
 
 type Params = {
     id: string
 }
+const Topbar = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background:white;
+`;
 const Tag: React.FC = (props) => {
     const {findTag} = useTags();
     let {id} = useParams<Params>();   //params 得到的id 是路由的:id 这是一个string
     const tag = findTag(parseInt(id));
     return (
         <Layout>
-            <header>
+            <Topbar>
                 <Icon name="left"/>
                 <span>编辑标签</span>
-            </header>
+                <Icon/>
+            </Topbar>
             <div>
                 <label>
-                    <span>备注</span>
-                    <input type="text" placeholder="请输入备注"/>
+                    <span>标签名</span>
+                    <input type="text" placeholder="请输入"/>
                 </label>
             </div>
             <div>

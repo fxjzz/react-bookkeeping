ad41b7d61ba902d5ae76186da3345cca69c874c0
lzxzz <1042183935@qq.com>
2022-10-21 19:27:39 +0800

    完善时间 显示

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,41 +1,23 @@
 import Layout from "../components/Layout";
-import React, {useState} from "react";
-import CategorySection from "./Money/CategorySection";
-import styled from "styled-components";
+import React from "react";
 import {RecordItem, useRecords} from "../hooks/useRecords";
 import useTags from "../hooks/useTags";
 import dayjs from "dayjs";
-
-const CategoryWrapper = styled.div`
-  background: white;
-`;
-const Item = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background: white;
-  font-size: 18px;
-  line-height: 20px;
-  padding: 10px 16px;
-
-  > .note {
-    margin-right: auto;
-    margin-left: 16px;
-    color: #999;
-  }
-`;
-const Header = styled.h3`
-  font-size: 18px;
-  line-height: 20px;
-  padding: 10px 16px;
-`;
+import logo from 'bookkeeping.jpg';
+import {Item, Header, DivImg, Img, TotalInfo, Calendar, Amount,} from './Statistics/StatisticsCSS';
 
 function Statistics() {
-    const [category, setCategory] = useState<'-' | '+'>('-');
     const {records} = useRecords();
     const {getName} = useTags();
     const hash: { [key: string]: RecordItem[] } = {};
-    const selectedRecords = records.filter(r => r.category === category);
-    selectedRecords.forEach(r => {
+    const utc = require('dayjs/plugin/utc');
+    let income:number=0
+    let expend:number=0
+    records.forEach(r=>{r.category==='-'? income+=r.amount: expend+=r.amount})
+    dayjs.extend(utc);
+    const yesterday = dayjs().subtract(1, 'day').format('YYYY年MM月DD日')
+    const today =dayjs().format('YYYY年MM月DD日')
+    records.forEach(r => {
         const key = dayjs(r.createdAt).format('YYYY年MM月DD日');
         if (!(key in hash)) {
             hash[key] = [];
@@ -50,28 +32,41 @@ function Statistics() {
     });
     return (
         <Layout>
-            <CategoryWrapper>
-                <CategorySection value={category}
-                                 onChange={value => {
-                                     setCategory(value);
-                                 }}/>
-            </CategoryWrapper>
+            <DivImg>
+                <Img src={logo} alt={logo}></Img>
+            </DivImg>
+            <TotalInfo>
+                <Calendar>
+                    <div>{dayjs().format('YYYY年')}</div>
+                    <span>{dayjs().format('MM月')}</span>
+                </Calendar>
+                <Amount>
+                    <div>
+                        <div>收入</div>
+                        <span>{income}</span>
+                    </div>
+                    <div>
+                        <div>支出</div>
+                        <span>{expend}</span>
+                    </div>
+                </Amount>
+            </TotalInfo>
             {arr.map(([date, records]) =>
                 <div key={date}>
                     <Header>
-                        {date}
+                        {date===today?'今天':(date===yesterday?'昨天':date)}
                     </Header>
                     <div>
                         {records.map(r => {
                             return <Item key={r.createdAt}>
                                 <div className="tags oneLine">
-                                    {<span key={r.tagId}>{getName(r.tagId,r.category)}</span>}
+                                    {<span key={r.tagId}>{getName(r.tagId, r.category)}</span>}
                                 </div>
                                 {r.note && <div className="note">
                                     {r.note}
                                 </div>}
                                 <div className="amount">
-                                    ￥{r.amount}
+                                    {r.category}￥{r.amount}
                                 </div>
                             </Item>;
                         })}
---
+++ src/views/Statistics/StatisticsCSS.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Item = styled.a`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+
+  > .note {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+`;
+
+export const Header = styled.h3`
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+`;
+export const DivImg = styled.div`
+  height: 30px;
+  background: #ffda47;
+  display: flex;
+  justify-content: center;
+`;
+export const Img = styled.img`
+  height: 30px;
+`;
+export const TotalInfo = styled.div`
+  padding-top:8px;
+  height: 59px;
+  background: #ffda47;
+  display: grid;
+  grid-template-columns: 100px 1fr;
+`;
+export const Calendar = styled.div`
+  position: relative;
+  >div{
+    text-align: center;
+    font-size: 16px;
+  }
+  >span{
+    display: block;
+    text-align: center;
+    font-size: 20px;
+  }
+  ::after {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    content: '';
+    height: 24px;
+    width: 1px;
+    background: #333;
+  }
+`;
+export const Amount = styled.div`
+  padding-left: 16px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
